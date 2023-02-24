920d16642e3e85e5aab053bed8c8d7c7e7c53ed6
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-02-25 01:45:32 +0900

    relation

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -19,6 +19,29 @@
     </h1>
     <p>{!! nl2br(e($post->body)) !!}</p>
 
+    <h2>Comments</h2>
+    <ul>
+        <li>
+            <form method="post" action="{{ route('comments.store', $post) }}" class="comment-form">
+                @csrf
+
+                <input type="text" name="body">
+                <button>Add</button>
+            </form>
+        </li>
+        @foreach ($post->comments()->latest()->get() as $comment)
+            <li>
+                {{ $comment->body }}
+                <form method="post" action="{{ route('comments.destroy', $comment) }}" class="delete-comment">
+                    @method('DELETE')
+                    @csrf
+
+                    <button class="btn">[x]</button>
+                </form>
+            </li>
+        @endforeach
+    </ul>
+
     <script>
         'use strict';
 
@@ -31,6 +54,18 @@
                 }
 
                 e.target.submit();
+            });
+
+            document.querySelectorAll('.delete-comment').forEach(form => {
+                form.addEventListener('submit', e => {
+                    e.preventDefault();
+
+                    if (!confirm('Sure to delete?')) {
+                        return;
+                    }
+
+                    form.submit();
+                });
             });
         }
     </script>
